d0c18e8a05d616671ecc79a644a3fce8a63d0fa3
cody <cody.brown1993@gmail.com>
2021-03-18 17:45:13 -0400

    09 Completed

--- scopeProject/problems/09-interrupter.js
+++ scopeProject/problems/09-interrupter.js
@@ -6,7 +6,21 @@ is invoked with a string the string will be returned with "interruptions".
 
 
 
+let interrupter = (interruptingWord) => {
 
+  return (str) => {
+    let sent = ""
+    let words = str.split(" ")
+    for (let i = 0; i < words.length - 1; i++) {
+      let word = words[i]
+
+      sent += word + ' ' + interruptingWord + ' '
+    }
+    sent += words[words.length - 1]
+    return sent
+  }
+
+}
 
 
 
