5ab41ded2c1ca7dc04ecd1df2469b78525e15c6d
Scilaer <Scilaerg@gmail.com>
2021-03-18 11:30:19 -0500

    first commit

---
+++ scopeProject/problems/00-arrow-addfive.js
@@ -0,0 +1,27 @@
+/***********************************************************************
+Write a function `addFive` that accepts a number and will return that number
+plus 5. Write this function on a single line, and utilize a fat arrow function's 
+ability to implicitly return by leaving out your own return statement.
+***********************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+console.log( result1 = addFive(0) ); // returns 5
+console.log( result2 = addFive(10) ); // returns 15
+console.log( result3 = addFive(37) ); // returns 42
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = addFive;
+} catch (e) {
+  return null;
+}
